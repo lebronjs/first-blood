bc979fa616934d45de9d09664c615c881c42cac0
Allen Xu <49184912@qq.com>
2019-09-18 14:47:46 +0800

    Create outlineDebug.js

---
+++ outlineDebug.js
@@ -0,0 +1,16 @@
+javascript:(function(){
+  var elements = document.body.getElementsByTagNames('*');
+  var items = [];
+  for (var i = 0; i<elements.length; i++){
+    if(elements[i].innerHTML.indexOf('html *{outline:1px solid red}')!=-1){
+      items.push(elements[i]);
+    }
+  }
+  if(item.length>0){
+    for(var i =0; i<items.length; i++){
+      items[i].innerHtml=''
+    }else{
+      document.body.innerHTML += '<style>html *{outline:1px solid red}</style>'
+    }
+  }
+})();
